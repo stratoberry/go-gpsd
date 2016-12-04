8870e4f296b4e2b399fc19e4d7fa38c6ef697f35
Josip Lisec <josip@jlx.cc>
2016-12-05 00:08:59 +0100

    docs and error handling

--- gpsd.go
+++ gpsd.go
@@ -1,196 +1,217 @@
 package gpsd
 
 import (
-  "bufio"
-  "encoding/json"
-  "fmt"
-  "net"
-  "time"
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"net"
+	"time"
 )
 
+// DefaultAddress of gpsd (localhost:2947)
 const DefaultAddress = "localhost:2947"
 
+// Filter is a gpsd entry filter function
 type Filter func(interface{})
 
+// Session represents a connection to gpsd
 type Session struct {
-  socket  net.Conn
-  reader  *bufio.Reader
-  filters map[string][]Filter
+	socket  net.Conn
+	reader  *bufio.Reader
+	filters map[string][]Filter
 }
 
+// Mode describes status of a TPV report
 type Mode byte
 
 const (
-  NoValueSeen Mode = 0
-  NoFix       Mode = 1
-  Mode2D      Mode = 2
-  Mode3D      Mode = 3
+	// NoValueSeen indicates no data has been received yet
+	NoValueSeen Mode = 0
+	// NoFix indicates fix has not been required yet
+	NoFix Mode = 1
+	// Mode2D represents quality of the fix
+	Mode2D Mode = 2
+	// Mode3D represents quality of the fix
+	Mode3D Mode = 3
 )
 
-type GPSDReport struct {
-  Class string `json:"class"`
+type gpsdReport struct {
+	Class string `json:"class"`
 }
 
+// TPVReport is a Time-Position-Velocity report
 type TPVReport struct {
-  Class  string    `json:"class"`
-  Tag    string    `json:"tag"`
-  Device string    `json:"device"`
-  Mode   Mode      `json:"mode"`
-  Time   time.Time `json:"time"`
-  Ept    float64   `json:"ept"`
-  Lat    float64   `json:"lat"`
-  Lon    float64   `json:"lon"`
-  Alt    float64   `json:"alt"`
-  Epx    float64   `json:"epx"`
-  Epy    float64   `json:"epy"`
-  Epv    float64   `json:"epv"`
-  Track  float64   `json:"track"`
-  Speed  float64   `json:"speed"`
-  Climb  float64   `json:"climb"`
-  Epd    float64   `json:"epd"`
-  Eps    float64   `json:"eps"`
-  Epc    float64   `json:"epc"`
+	Class  string    `json:"class"`
+	Tag    string    `json:"tag"`
+	Device string    `json:"device"`
+	Mode   Mode      `json:"mode"`
+	Time   time.Time `json:"time"`
+	Ept    float64   `json:"ept"`
+	Lat    float64   `json:"lat"`
+	Lon    float64   `json:"lon"`
+	Alt    float64   `json:"alt"`
+	Epx    float64   `json:"epx"`
+	Epy    float64   `json:"epy"`
+	Epv    float64   `json:"epv"`
+	Track  float64   `json:"track"`
+	Speed  float64   `json:"speed"`
+	Climb  float64   `json:"climb"`
+	Epd    float64   `json:"epd"`
+	Eps    float64   `json:"eps"`
+	Epc    float64   `json:"epc"`
 }
 
+// SKYReport reports sky view of GPS satellites
 type SKYReport struct {
-  Class      string      `json:"class"`
-  Tag        string      `json:"tag"`
-  Device     string      `json:"device"`
-  Time       time.Time   `json:"time"`
-  Xdop       float64     `json:"xdop"`
-  Ydop       float64     `json:"ydop"`
-  Vdop       float64     `json:"vdop"`
-  Tdop       float64     `json:"tdop"`
-  Hdop       float64     `json:"hdop"`
-  Pdop       float64     `json:"pdop"`
-  Gdop       float64     `json:"gdop"`
-  Satellites []Satellite `json:"satellites"`
+	Class      string      `json:"class"`
+	Tag        string      `json:"tag"`
+	Device     string      `json:"device"`
+	Time       time.Time   `json:"time"`
+	Xdop       float64     `json:"xdop"`
+	Ydop       float64     `json:"ydop"`
+	Vdop       float64     `json:"vdop"`
+	Tdop       float64     `json:"tdop"`
+	Hdop       float64     `json:"hdop"`
+	Pdop       float64     `json:"pdop"`
+	Gdop       float64     `json:"gdop"`
+	Satellites []Satellite `json:"satellites"`
 }
 
+// GSTReport is pseudorange noise report
 type GSTReport struct {
-  Class  string    `json:"class"`
-  Tag    string    `json:"tag"`
-  Device string    `json:"device"`
-  Time   time.Time `json:"time"`
-  Rms    float64   `json:"rms"`
-  Major  float64   `json:"major"`
-  Minor  float64   `json:"minor"`
-  Orient float64   `json:"orient"`
-  Lat    float64   `json:"lat"`
-  Lon    float64   `json:"lon"`
-  Alt    float64   `json:"alt"`
+	Class  string    `json:"class"`
+	Tag    string    `json:"tag"`
+	Device string    `json:"device"`
+	Time   time.Time `json:"time"`
+	Rms    float64   `json:"rms"`
+	Major  float64   `json:"major"`
+	Minor  float64   `json:"minor"`
+	Orient float64   `json:"orient"`
+	Lat    float64   `json:"lat"`
+	Lon    float64   `json:"lon"`
+	Alt    float64   `json:"alt"`
 }
 
+// ATTReport reports vehicle-attitude from the digital compass or the gyroscope
 type ATTReport struct {
-  Class       string    `json:"class"`
-  Tag         string    `json:"tag"`
-  Device      string    `json:"device"`
-  Time        time.Time `json:"time"`
-  Heading     float64   `json:"heading"`
-  MagSt       string    `json:"mag_st"`
-  Pitch       float64   `json:"pitch"`
-  PitchSt     string    `json:"pitch_st"`
-  Yaw         float64   `json:"yaw"`
-  YawSt       string    `json:"yaw_st"`
-  Roll        float64   `json:"roll"`
-  RollSt      string    `json:"roll_st"`
-  Dip         float64   `json:"dip"`
-  MagLen      float64   `json:"mag_len"`
-  MagX        float64   `json:"mag_x"`
-  MagY        float64   `json:"mag_y"`
-  MagZ        float64   `json:"mag_z"`
-  AccLen      float64   `json:"acc_len"`
-  AccX        float64   `json:"acc_x"`
-  AccY        float64   `json:"acc_y"`
-  AccZ        float64   `json:"acc_z"`
-  GyroX       float64   `json:"gyro_x"`
-  GyroY       float64   `json:"gyro_y"`
-  Depth       float64   `json:"depth"`
-  Temperature float64   `json:"temperature"`
+	Class       string    `json:"class"`
+	Tag         string    `json:"tag"`
+	Device      string    `json:"device"`
+	Time        time.Time `json:"time"`
+	Heading     float64   `json:"heading"`
+	MagSt       string    `json:"mag_st"`
+	Pitch       float64   `json:"pitch"`
+	PitchSt     string    `json:"pitch_st"`
+	Yaw         float64   `json:"yaw"`
+	YawSt       string    `json:"yaw_st"`
+	Roll        float64   `json:"roll"`
+	RollSt      string    `json:"roll_st"`
+	Dip         float64   `json:"dip"`
+	MagLen      float64   `json:"mag_len"`
+	MagX        float64   `json:"mag_x"`
+	MagY        float64   `json:"mag_y"`
+	MagZ        float64   `json:"mag_z"`
+	AccLen      float64   `json:"acc_len"`
+	AccX        float64   `json:"acc_x"`
+	AccY        float64   `json:"acc_y"`
+	AccZ        float64   `json:"acc_z"`
+	GyroX       float64   `json:"gyro_x"`
+	GyroY       float64   `json:"gyro_y"`
+	Depth       float64   `json:"depth"`
+	Temperature float64   `json:"temperature"`
 }
 
+// VERSIONReport returns version details of gpsd client
 type VERSIONReport struct {
-  Class      string `json:"class"`
-  Release    string `json:"release"`
-  Rev        string `json:"rev"`
-  ProtoMajor int    `json:"proto_major"`
-  ProtoMinor int    `json:"proto_minor"`
-  Remote     string `json:"remote"`
+	Class      string `json:"class"`
+	Release    string `json:"release"`
+	Rev        string `json:"rev"`
+	ProtoMajor int    `json:"proto_major"`
+	ProtoMinor int    `json:"proto_minor"`
+	Remote     string `json:"remote"`
 }
 
+// DEVICESReport lists all devices connected to the system
 type DEVICESReport struct {
-  Class   string         `json:"class"`
-  Devices []DEVICEReport `json:"devices"`
-  Remote  string         `json:"remote"`
+	Class   string         `json:"class"`
+	Devices []DEVICEReport `json:"devices"`
+	Remote  string         `json:"remote"`
 }
 
+// DEVICEReport reports a state of a particular device
 type DEVICEReport struct {
-  Class     string  `json:"class"`
-  Path      string  `json:"path"`
-  Activated string  `json:"activated"`
-  Flags     int     `json:"flags"`
-  Driver    string  `json:"driver"`
-  Subtype   string  `json:"subtype"`
-  Bps       int     `json:"bps"`
-  Parity    string  `json:"parity"`
-  Stopbits  string  `json:"stopbits"`
-  Native    int     `json:"native"`
-  Cycle     float64 `json:"cycle"`
-  Mincycle  float64 `json:"mincycle"`
+	Class     string  `json:"class"`
+	Path      string  `json:"path"`
+	Activated string  `json:"activated"`
+	Flags     int     `json:"flags"`
+	Driver    string  `json:"driver"`
+	Subtype   string  `json:"subtype"`
+	Bps       int     `json:"bps"`
+	Parity    string  `json:"parity"`
+	Stopbits  string  `json:"stopbits"`
+	Native    int     `json:"native"`
+	Cycle     float64 `json:"cycle"`
+	Mincycle  float64 `json:"mincycle"`
 }
 
+// PPSReport is triggered on each pulse-per-second strobe from a device
 type PPSReport struct {
-  Class      string  `json:"class"`
-  Device     string  `json:"device"`
-  RealSec    float64 `json:"real_sec"`
-  RealMusec  float64 `json:"real_musec"`
-  ClockSec   float64 `json:"clock_sec"`
-  ClockMusec float64 `json:"clock_musec"`
+	Class      string  `json:"class"`
+	Device     string  `json:"device"`
+	RealSec    float64 `json:"real_sec"`
+	RealMusec  float64 `json:"real_musec"`
+	ClockSec   float64 `json:"clock_sec"`
+	ClockMusec float64 `json:"clock_musec"`
 }
 
+// ERRORReport is an error response
 type ERRORReport struct {
-  Class   string `json:"class"`
-  Message string `json:"message"`
+	Class   string `json:"class"`
+	Message string `json:"message"`
 }
 
+// Satellite describes a location of a GPS satellite
 type Satellite struct {
-  PRN  float64 `json:"PRN"`
-  Az   float64 `json:"az"`
-  El   float64 `json:"el"`
-  Ss   float64 `json:"ss"`
-  Used bool    `json:"used"`
+	PRN  float64 `json:"PRN"`
+	Az   float64 `json:"az"`
+	El   float64 `json:"el"`
+	Ss   float64 `json:"ss"`
+	Used bool    `json:"used"`
 }
 
 // Dial opens a new connection to GPSD.
 func Dial(address string) (session *Session, err error) {
-  session = new(Session)
-  session.socket, err = net.Dial("tcp4", address)
+	session = new(Session)
+	session.socket, err = net.Dial("tcp4", address)
+	if err != nil {
+		return nil, err
+	}
 
-  session.reader = bufio.NewReader(session.socket)
-  line, _ := session.reader.ReadString('\n')
-  _ = line
-  session.filters = make(map[string][]Filter)
+	session.reader = bufio.NewReader(session.socket)
+	session.reader.ReadString('\n')
+	session.filters = make(map[string][]Filter)
 
-  return
+	return
 }
 
-// Starts watching GPSD reports in a new goroutine.
+// Watch starts watching GPSD reports in a new goroutine.
 //
 // Example
 //    gps := gpsd.Dial(gpsd.DEFAULT_ADDRESS)
 //    done := gpsd.Watch()
 //    <- done
 func (s *Session) Watch() (done chan bool) {
-  fmt.Fprintf(s.socket, "?WATCH={\"enable\":true,\"json\":true}")
-  done = make(chan bool)
+	fmt.Fprintf(s.socket, "?WATCH={\"enable\":true,\"json\":true}")
+	done = make(chan bool)
 
-  go watch(done, s)
+	go watch(done, s)
 
-  return
+	return
 }
 
+// SendCommand sends a command to GPSD
 func (s *Session) SendCommand(command string) {
-  fmt.Fprintf(s.socket, "?"+command+";")
+	fmt.Fprintf(s.socket, "?"+command+";")
 }
 
 // AddFilter attaches a function which will be called for all
@@ -205,80 +226,78 @@ func (s *Session) SendCommand(command string) {
 //    done := gps.Watch()
 //    <- done
 func (s *Session) AddFilter(class string, f Filter) {
-  s.filters[class] = append(s.filters[class], f)
+	s.filters[class] = append(s.filters[class], f)
 }
 
 func (s *Session) deliverReport(class string, report interface{}) {
-  for _, f := range s.filters[class] {
-    f(report)
-  }
+	for _, f := range s.filters[class] {
+		f(report)
+	}
 }
 
 func watch(done chan bool, s *Session) {
-  fmt.Println("WATCHING")
+	// We're not using a JSON decoder because we first need to inspect
+	// the JSON string to determine it's "class"
+	for {
+		if line, err := s.reader.ReadString('\n'); err == nil {
+			var reportPeek gpsdReport
+			lineBytes := []byte(line)
+			if err = json.Unmarshal(lineBytes, &reportPeek); err == nil {
+				if len(s.filters[reportPeek.Class]) == 0 {
+					continue
+				}
 
-  // We're not using a JSON decoder because we first need to inspect
-  // the JSON string to determine it's "class"
-  for {
-    if line, err := s.reader.ReadString('\n'); err == nil {
-      var reportPeek GPSDReport
-      lineBytes := []byte(line)
-      if err = json.Unmarshal(lineBytes, &reportPeek); err == nil {
-        if len(s.filters[reportPeek.Class]) == 0 {
-          continue
-        }
-
-        if report, err := unmarshalReport(reportPeek.Class, lineBytes); err == nil {
-          s.deliverReport(reportPeek.Class, report)
-        } else {
-          fmt.Println("JSON parsing error 2:", err)
-        }
-      } else {
-        fmt.Println("JSON parsing error:", err)
-      }
-    } else {
-      fmt.Println("Stream reader error:", err)
-    }
-  }
+				if report, err2 := unmarshalReport(reportPeek.Class, lineBytes); err2 == nil {
+					s.deliverReport(reportPeek.Class, report)
+				} else {
+					fmt.Println("JSON parsing error 2:", err)
+				}
+			} else {
+				fmt.Println("JSON parsing error:", err)
+			}
+		} else {
+			fmt.Println("Stream reader error (is gpsd running?):", err)
+		}
+	}
 }
 
 func unmarshalReport(class string, bytes []byte) (interface{}, error) {
-  var err error
+	var err error
 
-  switch class {
-  case "TPV":
-    var r *TPVReport
-    err = json.Unmarshal(bytes, &r)
-    return r, err
-  case "SKY":
-    var r *SKYReport
-    err = json.Unmarshal(bytes, &r)
-    return r, err
-  case "GST":
-    var r *GSTReport
-    err = json.Unmarshal(bytes, &r)
-    return r, err
-  case "ATT":
-    var r *ATTReport
-    err = json.Unmarshal(bytes, &r)
-    return r, err
-  case "VERSION":
-    var r *VERSIONReport
-    err = json.Unmarshal(bytes, &r)
-    return r, err
-  case "DEVICES":
-    var r *DEVICESReport
-    err = json.Unmarshal(bytes, &r)
-    return r, err
-  case "PPS":
-    var r *PPSReport
-    err = json.Unmarshal(bytes, &r)
-    return r, err
-  case "ERROR":
-    var r *ERRORReport
-    err = json.Unmarshal(bytes, &r)
-    return r, err
-  }
+	switch class {
+	case "TPV":
+		var r *TPVReport
+		err = json.Unmarshal(bytes, &r)
+		return r, err
+	case "SKY":
+		var r *SKYReport
+		err = json.Unmarshal(bytes, &r)
+		return r, err
+	case "GST":
+		var r *GSTReport
+		err = json.Unmarshal(bytes, &r)
+		return r, err
+	case "ATT":
+		var r *ATTReport
+		err = json.Unmarshal(bytes, &r)
+		return r, err
+	case "VERSION":
+		var r *VERSIONReport
+		err = json.Unmarshal(bytes, &r)
+		return r, err
+	case "DEVICES":
+		var r *DEVICESReport
+		err = json.Unmarshal(bytes, &r)
+		return r, err
+	case "PPS":
+		var r *PPSReport
+		err = json.Unmarshal(bytes, &r)
+		return r, err
+	case "ERROR":
+		var r *ERRORReport
+		err = json.Unmarshal(bytes, &r)
+		return r, err
+	}
 
-  return nil, err
+	return nil, err
 }
