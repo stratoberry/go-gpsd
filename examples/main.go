54ddcfa61f471b13a707324102a5c1f44c3b2910
Josip Lisec <josip@jlx.cc>
2016-12-05 00:11:41 +0100

    example: formatting

--- examples/main.go
+++ examples/main.go
@@ -1,31 +1,28 @@
 package main
 
-import (
-  "fmt"
-  "github.com/stratoberry/go-gpsd"
-)
+import "fmt"
 
 func main() {
-  var gps *gpsd.Session
-  var err error
+	var gps *gpsd.Session
+	var err error
 
-  if gps, err = gpsd.Dial(gpsd.DefaultAddress); err != nil {
-    panic(fmt.Sprintf("Failed to connect to GPSD: ", err))
-  }
+	if gps, err = gpsd.Dial(gpsd.DefaultAddress); err != nil {
+		panic(fmt.Sprintf("Failed to connect to GPSD: %s", err))
+	}
 
-  gps.AddFilter("TPV", func(r interface{}) {
-    tpv := r.(*gpsd.TPVReport)
-    fmt.Println("TPV", tpv.Mode, tpv.Time)
-  })
+	gps.AddFilter("TPV", func(r interface{}) {
+		tpv := r.(*gpsd.TPVReport)
+		fmt.Println("TPV", tpv.Mode, tpv.Time)
+	})
 
-  skyfilter := func(r interface{}) {
-    sky := r.(*gpsd.SKYReport)
+	skyfilter := func(r interface{}) {
+		sky := r.(*gpsd.SKYReport)
 
-    fmt.Println("SKY", len(sky.Satellites), "satellites")
-  }
+		fmt.Println("SKY", len(sky.Satellites), "satellites")
+	}
 
-  gps.AddFilter("SKY", skyfilter)
+	gps.AddFilter("SKY", skyfilter)
 
-  done := gps.Watch()
-  <-done
+	done := gps.Watch()
+	<-done
 }
